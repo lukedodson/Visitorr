8b143f13ad494008aa2f5fb516183b3f317705c1
John-Henry Liberty <johnhenry.liberty@gmail.com>
2011-11-16 05:33:40 -0600

    fixed sessions controller to handle bad passwords.. newb.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,6 +8,7 @@ class SessionsController < ApplicationController
       redirect_to admin_path, :notice => "Logged in!"
     else
       flash.now.alert = "Email or password was invalid."
+      render :new
     end
   end
   
