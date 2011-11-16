40ee76f7de45b2b9e9491ab7d4c1382ad37303f7
John-Henry Liberty <johnhenry.liberty@gmail.com>
2011-11-16 11:43:47 -0600

    commented out gmail settings

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,12 +1,12 @@
 require "development_mail_interceptor"
-ActionMailer::Base.smtp_settings = {  
-  :address              => "smtp.gmail.com",  
-  :port                 => 587,  
-  :domain               => "gmail.com",  
-  :user_name            => "visitorrdev@gmail.com",  
-  :password             => "lukedodson",  
-  :authentication       => "plain",  
-  :enable_starttls_auto => true  
-}  
+#ActionMailer::Base.smtp_settings = {  
+#  :address              => "smtp.gmail.com",  
+#  :port                 => 587,  
+#  :domain               => "gmail.com",  
+#  :user_name            => "visitorrdev@gmail.com",  
+#  :password             => "lukedodson",  
+#  :authentication       => "plain",  
+#  :enable_starttls_auto => true  
+#}  
 
 ActionMailer::Base.register_interceptor(DevelopmentMailInterceptor)
