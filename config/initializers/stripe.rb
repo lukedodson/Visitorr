0fb86584ab5636f8f390f44858395322da1e7ba2
John-Henry Liberty <johnhenry.liberty@gmail.com>
2011-11-16 05:00:11 -0600

    Updated stripe.rb and made some design changes to profile partial.

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,2 +1,7 @@
-Stripe.api_key = "hUflY7VTGTp30Ux95BhuKusaHmghBM3W"
-STRIPE_PUBLIC_KEY = "pk_zpM03X6BwMpTU7J8VnlxXgXPCj2k6"  
+# development test settings - johnhenry.liberty@gmail.com
+#Stripe.api_key = "bNkiv1IODUi0caelH9fJCZIQOEhAwXE5"
+#STRIPE_PUBLIC_KEY = "pk_1bBAOyEcLIiPQzHQv6Qck5SueApPy" 
+
+# production test settings - lukedodson@gmail.com
+Stripe.api_key = "3JeOUAMyRSOA19bV9AEZHR9qtvBU7SJS" 
+STRIPE_PUBLIC_KEY = "pk_yBQGz2OLWCiagaeAMDIMeXuXCBHC6" 
