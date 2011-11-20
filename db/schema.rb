0cd5693ef8c82ea6e6eed14810070a14f0510db0
John-Henry Liberty <johnhenry.liberty@gmail.com>
2011-11-19 21:19:41 -0600

    added profiles model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111120002243) do
+ActiveRecord::Schema.define(:version => 20111120015600) do
 
   create_table "delayed_jobs", :force => true do |t|
     t.integer  "priority",   :default => 0
@@ -27,6 +27,17 @@ ActiveRecord::Schema.define(:version => 20111120002243) do
   end
 
   add_index "delayed_jobs", ["priority", "run_at"], :name => "delayed_jobs_priority"
+
+  create_table "profiles", :force => true do |t|
+    t.string   "address"
+    t.string   "phone"
+    t.string   "video_url"
+    t.string   "subject"
+    t.text     "body"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.integer  "user_id"
+  end
 
   create_table "users", :force => true do |t|
     t.string   "email",                                           :null => false
