caec92ef34a2f30fef44006551ebd880c1a408bd
John-Henry Liberty <johnhenry.liberty@gmail.com>
2011-11-29 16:23:37 -0600

    momdified visitors mailer - changed subject line

--- app/mailers/visitor_mailer.rb
+++ app/mailers/visitor_mailer.rb
@@ -4,6 +4,7 @@ class VisitorMailer < ActionMailer::Base
   def welcome_mailer(visitor)
     visitorr = Visitor.find_by_id(visitor)
     @user = User.find_by_id(visitorr.user_id)
-    visitorr.update_attributes(:welcomed => true) if mail(:to => visitorr.email, :subject => @user.profile.subject || "Welcome to #{@user.name} ", :from => @user.email)  
+    @subject = @user.profile.subject || "Welcome to #{@user.name}"
+    visitorr.update_attributes(:welcomed => true) if mail(:to => visitorr.email, :subject => @subject, :from => @user.email)  
   end
 end
