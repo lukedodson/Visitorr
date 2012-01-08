38e6eb0b865db6a5d0705bb517198cbc3a63405b
John-Henry Liberty <johnhenry.liberty@gmail.com>
2012-01-08 16:01:30 -0600

    added notice when 0 visitors

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -5,5 +5,8 @@ class AdminController < ApplicationController
   def visitors
     @user = current_user
     @visitors = @user.visitors.order("created_at desc")
+    if @visitors.count == 0
+      flash.now.notice = "Go to your profile and setup your welcome email to allow your visitors to sign up for more information!"
+    end
   end
 end
